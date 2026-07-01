0d1120fc222da60d74cbb4a9a095a0c27f3593f1
Hoang <nguyenduchoang522@gmail.com>
2026-07-01 18:35:59 +0700

    Cài đặt và cấu hình Alpine Collapse plugin để sửa lỗi cảnh báo x-collapse

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,9 @@ import './elements/turbo-echo-stream-tag';
 import './libs';
 
 import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
+
+Alpine.plugin(collapse);
+
 window.Alpine = Alpine;
 Alpine.start();
